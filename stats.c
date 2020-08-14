28b6c01040122e3fa9bee9a1ffa5cb8d95bcdb1b
Greg <igmemb0@gmail.com>
2020-08-13 19:49:05 -0700

    Added logic to code for the header functions.

--- stats.c
+++ stats.c
@@ -27,20 +27,122 @@
 /* Size of the Data Set */
 #define SIZE (40)
 
-void main() {
+void main()
+{
 
   unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
                               114, 88,   45,  76, 123,  87,  25,  23,
                               200, 122, 150, 90,   92,  87, 177, 244,
                               201,   6,  12,  60,   8,   2,   5,  67,
                                 7,  87, 250, 230,  99,   3, 100,  90};
+  sort_array(test,size);
+  print_array(test,size);
+  find_mean(test, size);
+  find_median(test,size);
+  find_maximum(test,size);
+  find_minimum(test,size);
+  print_statistics(test,size);
 
   /* Other Variable Declarations Go Here */
   /* Statistics and Printing Functions Go Here */
 
 }
 
+
 /* Add other Implementation File Code Here */
 float find_mean(unsigned char test[], int size)
 {
+  int sum = 0;
+  float mean = 0;
+  
+  for(int i = 0; i <= SIZE; i++)
+  {
+      sum = sum + test[i];
+  }
+  return (mean = sum/SIZE);
 }
+
+void sort_array(unsigned char *test[], int size)
+{
+  int temp = 0; // short for temporary
+
+  for(int i = 0; i < SIZE; i++) // starts from first element
+    {
+      for(int j = i + 1; j < SIZE; j++) // starts from second element
+	{
+	  if (test[i] < test[j]) // checks if the first element is less than the next element.
+	    {
+	      temp = test[i]; // assign value of ith element to temp
+	      test[i] = test[j]; // assign value of jth element to i
+	      test[j] = temp; // assign value of temp to test j
+	    }
+	}
+    }
+  
+}
+
+float find_median(unsigned char test[], int size)
+{
+  
+  float median = 0;
+  
+  median = (test[(n-1)]/2] + test[n/2])/2.0; /* We know that the array is even so I don't need other logic */
+
+  return median;
+
+}
+
+float find_maximum(unsigned char test[], int size)
+{
+  maximum = test[0];
+  for( int i = 0; i < SIZE: i++)
+    {
+      if(test[i] > maximum)
+	{
+	  maximum = test[i];
+	  i++ // this makes it so the for loop starts again but at a value farther ahead
+	     // to start comparing again
+	}
+    }
+  return maximum
+
+}
+float find_minimum(unsigned char test[], int size)
+{
+  minimum = test[0];
+  for( int i = 1; i < SIZE: i++)
+    {
+      if(test[i] < minimum)
+        {
+          minimum = test[i];
+	  i++; // this makes it so the for loop starts again but at a value farther ahead
+	      // to start comparing again. 
+	}
+    }
+  return minimum
+
+}
+void print_statistics(float mean, float median, float max, float min)
+{
+  printf("\n\nThe following are the value of mean, median, maximum and minimum for the given\n unsigned  char array 'test'\n\n");
+  printf("The value of mean is = %.2f \n\n",mean);
+  printf("The value of median is = %.2f \n\n",median);
+  printf("The maximum value is = %d \n\n",max);
+  printf("The minimum value is = %d \n\n",min); 
+}
+void print_array(unsigned char test[], int size)
+{
+  printf("\nThe array after sorting is..\n");
+  for(i = 0; i < SIZE; i++)
+    {
+      printf("The element %d  in the array 'test' is %d\n\n",i,test[i]);
+    }
+}
+
+
+
+
+
+
+
+
